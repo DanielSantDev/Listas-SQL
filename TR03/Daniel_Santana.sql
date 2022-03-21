0be2fad60d69091e8b316515fb56337fce800231
Daniel Santana <daniel10gja@gmail.com>
2022-03-20 21:58:05 -0300

    Update TR03

--- TR03/Daniel_Santana.sql
+++ TR03/Daniel_Santana.sql
@@ -1,3 +1,23 @@
 USE bdimobiliaria;
+SELECT * FROM Comprador;
 
-SELECT * FROM Bairro;
+/*1*/
+SELECT cd_Comprador, nm_Comprador, ds_Email 
+FROM Comprador;
+
+/*2*/
+SELECT cd_Vendedor, nm_Vendedor, ds_Email
+FROM Vendedor
+ORDER BY nm_Vendedor DESC;
+
+/*3*/
+SELECT cd_Imovel, cd_Vendedor, vl_Preco
+FROM Imovel
+WHERE cd_Vendedor = 2;
+
+/*4*/
+SELECT cd_Imovel, cd_Vendedor, vl_Preco, sg_Estado
+FROM Imovel
+
+
+/*5*/
