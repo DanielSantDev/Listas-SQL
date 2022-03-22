d417f66fec325841026191a5252e2c73f19a56dc
Daniel Santana <daniel10gja@gmail.com>
2022-03-21 22:41:18 -0300

    Update Daniel_Santana.sql

--- TR03/Daniel_Santana.sql
+++ TR03/Daniel_Santana.sql
@@ -18,6 +18,48 @@ WHERE cd_Vendedor = 2;
 /*4*/
 SELECT cd_Imovel, cd_Vendedor, vl_Preco, sg_Estado
 FROM Imovel
-
+WHERE vl_Preco < 150000.00 AND sg_Estado = 'RJ';
 
 /*5*/
+SELECT cd_Imovel, cd_Vendedor, vl_Preco, sg_Estado
+FROM Imovel
+WHERE vl_Preco < 150000.00 AND cd_Vendedor != 2;
+
+/*6*/
+SELECT cd_Comprador, nm_Comprador, ds_Endereco, sg_Estado
+FROM Comprador
+WHERE sg_Estado IS NULL;
+
+/*7*/
+SELECT cd_Comprador, cd_Imovel, vl_Oferta, dt_Oferta
+FROM Oferta
+WHERE vl_Oferta > 100000.00 AND vl_Oferta < 150000.00;
+
+/*8*/
+SELECT dt_Oferta AS DataOferta
+FROM Oferta
+WHERE dt_Oferta BETWEEN "2009-01-01" AND "2009-03-01";
+
+/*9*/
+SELECT nm_Vendedor
+FROM Vendedor
+WHERE nm_Vendedor LIKE 'M%';
+
+/*10*/
+SELECT nm_Vendedor
+FROM Vendedor
+WHERE nm_Vendedor LIKE '_A%';
+
+/*11*/
+
+/*12*/
+
+/*13*/
+
+/*14*/
+
+/*15*/
+
+/*16*/
+
+/*17*/
