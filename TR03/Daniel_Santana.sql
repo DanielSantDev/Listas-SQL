2c2083e467ff9cac8971448c975c8a34bb23d964
Daniel Santana <daniel10gja@gmail.com>
2022-03-27 19:23:51 -0300

    Update Daniel_Santana.sql

--- TR03/Daniel_Santana.sql
+++ TR03/Daniel_Santana.sql
@@ -1,5 +1,4 @@
 USE bdimobiliaria;
-SELECT * FROM Imovel;
 
 /*1*/
 SELECT cd_Comprador, nm_Comprador, ds_Email 
@@ -23,7 +22,7 @@ WHERE (vl_Preco < 150000.00 AND sg_Estado = 'RJ');
 /*5*/
 SELECT cd_Imovel, cd_Vendedor, vl_Preco, sg_Estado
 FROM Imovel
-WHERE (vl_Preco < 150000.00 AND cd_Vendedor != 2);
+WHERE (vl_Preco < 150000.00  AND cd_Vendedor != 2);
 
 /*6*/
 SELECT cd_Comprador, nm_Comprador, ds_Endereco, sg_Estado
@@ -31,27 +30,28 @@ FROM Comprador
 WHERE sg_Estado IS NULL;
 
 /*7*/
-SELECT cd_Comprador, cd_Imovel, vl_Oferta, dt_Oferta
+SELECT cd_Comprador, cd_Imovel, vl_Oferta as OFERTA
 FROM Oferta
-WHERE (vl_Oferta > 100000.00 AND vl_Oferta < 150000.00);
+WHERE (vl_Oferta BETWEEN 100000 AND 150000);
+/*(vl_Oferta > 100000.00 AND vl_Oferta < 150000.00)*/
 
 /*8*/
-SELECT dt_Oferta AS DataOferta
+SELECT vl_Oferta, dt_Oferta AS DataOferta
 FROM Oferta
 WHERE (dt_Oferta BETWEEN "2009-01-01" AND "2009-03-01");
 
 /*9*/
-SELECT nm_Vendedor
+SELECT nm_Vendedor as VENDEDOR
 FROM Vendedor
 WHERE nm_Vendedor LIKE 'M%';
 
 /*10*/
-SELECT nm_Vendedor
+SELECT nm_Vendedor as VENDEDOR
 FROM Vendedor
 WHERE nm_Vendedor LIKE '_A%';
 
 /*11*/
-SELECT nm_Vendedor
+SELECT nm_Vendedor as VENDEDOR, ds_Endereco as ENDERECO
 FROM Vendedor
 WHERE ds_Endereco LIKE '%U%';
 
@@ -59,20 +59,34 @@ WHERE ds_Endereco LIKE '%U%';
 SELECT cd_imovel, ds_Endereco
 FROM Imovel
 WHERE (cd_imovel = 2 OR cd_imovel = 3)
-ORDER BY ds_Endereco ASC;
+ORDER BY ds_Endereco;
 
 /*13*/
 SELECT vl_Oferta, dt_Oferta, cd_Imovel
-FROM oferta
+FROM Oferta
 WHERE (cd_imovel = 2 OR cd_imovel = 3)
 AND (vl_Oferta > 140000)
 ORDER BY dt_Oferta DESC;
 
 /*14*/
-
+SELECT cd_Imovel, qt_AreaUtil, vl_Preco AS PrecoDeVenda
+FROM Imovel
+WHERE (vl_Preco BETWEEN 110000 AND 200000)
+ORDER BY qt_AreaUtil;
+/*(vl_Preco >= 110000) AND (vl_Preco <= 200000)*/
 
 /*15*/
+SELECT MAX(vl_Oferta) as MaiorOferta, MIN(vl_Oferta) as MenorOferta, AVG(vl_Oferta) as MediaOferta 
+FROM Oferta;
 
 /*16*/
+SELECT MAX(vl_Preco) as MaiorPreco, MIN(vl_Preco) as MenorPreco, AVG(vl_Preco) as MediaPreco 
+FROM Imovel;
 
 /*17*/
+SELECT SUM(qt_Ofertas) as TotalOfertas, dt_Oferta
+FROM Imovel, Oferta
+WHERE dt_Oferta BETWEEN "2008-01-01" AND "2010-12-31"
+GROUP BY dt_Oferta;
+
+SELECT * FROM Imovel;
