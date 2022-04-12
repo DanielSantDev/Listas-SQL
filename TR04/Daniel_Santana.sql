b022ce96051f32fe7206b7c0e2f8c8f56cca6449
Daniel Santana <daniel10gja@gmail.com>
2022-04-11 21:57:37 -0300

    Update Daniel_Santana.sql

--- TR04/Daniel_Santana.sql
+++ TR04/Daniel_Santana.sql
@@ -1,5 +1,4 @@
 /*TR04*/
-select * from Comprador;
 
 /*01*/
 SELECT cd_Imovel, Vendedor.cd_Vendedor, nm_Vendedor, Imovel.sg_Estado
@@ -42,3 +41,35 @@ RIGHT OUTER JOIN Comprador
 ON Oferta.cd_Comprador = Comprador.cd_Comprador;
 
 /*8*/
+SELECT ds_Endereco, nm_Bairro, vl_Maximo, vl_Minimo 
+FROM Imovel, Bairro, Faixa_Imovel
+WHERE Imovel.cd_Bairro = Bairro.cd_Bairro
+ORDER BY ds_Endereco ASC;
+
+SELECT COUNT(*) 
+FROM Vendedor;
+
+/*9*/
+SELECT COUNT(*) AS Total_Imovel, Vendedor.nm_Vendedor FROM Imovel 
+INNER JOIN Vendedor
+ON Imovel.cd_Vendedor = Vendedor.cd_Vendedor
+GROUP BY Vendedor.nm_Vendedor
+ORDER BY Total_Imovel ASC;
+
+/*10*/
+SELECT MAX(vl_Preco) AS 'MAX', MIN(vl_Preco) AS 'MIN',
+(MAX(vl_Preco) - MIN(vl_Preco)) AS 'DIFERENCA'
+FROM Imovel;
+
+/*11*/
+SELECT cd_Vendedor, MIN(vl_Preco)
+FROM Imovel
+GROUP BY cd_Vendedor
+HAVING MIN(vl_Preco) > 10000;
+
+/*12*/
+SELECT Oferta.cd_Comprador, nm_Comprador, AVG(vl_Oferta) AS MEDIA, COUNT(*) AS qtd_Ofertas
+FROM Oferta
+RIGHT OUTER JOIN Comprador
+ON Comprador.cd_Comprador = Oferta.cd_Comprador
+GROUP BY cd_Comprador;
